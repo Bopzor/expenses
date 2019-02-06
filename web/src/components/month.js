360af872514795e47e03ea64e105cee78f6e8695
Bopzor <violaine.ballandin@gmail.com>
2019-02-06 14:06:03 +0100

    use react router

--- web/src/components/month.js
+++ web/src/components/month.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
 
-import { Container, Row, Col } from 'reactstrap';
+import { Route, Switch } from "react-router-dom";
 
-import List from './list';
-import Total from './total';
+import { Table } from 'reactstrap';
+
+import ExpensesList from './containers/expensesList';
+import AdvancesList from './containers/advancesList';
+
+import Header from './header';
+
+import './month.css';
 
 class Month extends Component {
   render() {
+    const navPaths = [
+      {
+        pathname: "Expenses",
+        path: "/month"
+      },
+      {
+        pathname: "Advances",
+        path: "/month/advances"
+      },
+    ]
+
     return (
       <div>
-        <Container>
+        <Header date={this.props.dateFilter} navPaths={navPaths} changeDate={date => this.props.changeDate(date)} />
+        <Table size="sm" responsive>
 
-          <Row>
-            <Col>Date</Col>
-            <Col xs="6">Description</Col>
-            <Col>Cost</Col>
-            <Col></Col>
-          </Row>
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Description</th>
+              <th scope="col">Cost</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
 
-          <List />
+          <Switch>
+            <Route path="/month/advances" render={
+              props => <AdvancesList payementType="advance" dateFilter={this.props.dateFilter} />
+            } />
+            <Route path="/month" render={
+              props => <ExpensesList payementType="expense" dateFilter={this.props.dateFilter} />
+            } />
+          </Switch>
 
-        </Container>
+        </Table>
 
-        <Total />
       </div>
     );
   }
