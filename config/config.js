3286ef1a1f27c2f23e904a0f2b89c3e4c5573e6a
Bopzor <violaine.ballandin@gmail.com>
2019-03-03 17:46:54 +0100

    add test config

--- config/config.js
+++ config/config.js
@@ -15,4 +15,12 @@ module.exports = {
     dialect: 'postgres',
     operatorsAliases: false,
   },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_TEST,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: false,
+  },
 };
